e4fa59bc873c54160a82482b2b374d9140071f4c
Carlos Pedraza <carlosayb@hotmail.com>
2021-06-09 18:51:33 -0500

    Listo para subir

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-empleados';
+  titulo = 'Listado de Empleados';
+  empleados:Empleado[]=[
+    new Empleado("Carlos", "Pedraza", "Gerente", 2500000),
+    new Empleado("Pedro", "Arias", "Subgerente", 2000000),
+    new Empleado("Ana", "Suarez", "Contador", 1800000),
+    new Empleado("Oscar", "Gutierrez", "Auditor", 1900000),
+
+  ];
+
+  agregarEmpleado(){
+    let miEmpleado= new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario)
+    this.empleados.push(miEmpleado);
+  }
+
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0;
 }
